7beb9bc23c7429235be64827c044e8a899fdf503
zekun.jin <zekun.jin@connext.com.cn>
2022-10-28 16:29:36 +0800

    style: update user layout

--- components/user-layout.tsx
+++ components/user-layout.tsx
@@ -1,10 +1,21 @@
 import type { ReactNode } from 'react'
+import { Box } from '@mui/material'
+import Image from 'next/image'
+import illustrationDashboard from '../assets/illustration_dashboard.png'
 import Layout from './layout'
 
 const UserLayout = ({ children }: { children: ReactNode }) => (
   <Layout>
     <div className="w-screen h-screen flex flex-col items-center justify-center">
-      {children}
+      <Box className="w-screen h-screen flex">
+        <Box className="flex items-center justify-center flex-1 bg-gray-100">
+          <Image src={illustrationDashboard} alt="illustration dashboard" />
+        </Box>
+
+        <Box sx={{ width: '480px' }} className="h-full">
+          {children}
+        </Box>
+      </Box>
     </div>
   </Layout>
 )
--- pages/user/sign-in.tsx
+++ pages/user/sign-in.tsx
@@ -3,10 +3,8 @@ import Link from 'next/link'
 import { LoadingButton } from '@mui/lab'
 import { Box, TextField } from '@mui/material'
 import { useState } from 'react'
-import Image from 'next/image'
 import UserLayout from '../../components/user-layout'
 import { userModule } from '../../common/modules/user/user.module'
-import illustrationDashboard from '../../assets/illustration_dashboard.png'
 
 const UserSignIn = () => {
   const { username, password, signIn } = useModule(userModule)
@@ -20,30 +18,24 @@ const UserSignIn = () => {
 
   return (
     <UserLayout>
-      <Box className="w-screen h-screen flex">
-        <Box className="flex items-center justify-center flex-1 bg-gray-100">
-          <Image src={illustrationDashboard} alt="illustration dashboard" />
-        </Box>
-
-        <Box sx={{ width: '480px' }} className="flex flex-col items-center justify-center">
-          <Box className="w-full px-16 flex flex-col box-border">
-            <Box>
-              <h3>Sign In</h3>
-              <Box className="mt-4 text-xs">
-                <span>New user?</span>
-                <Link className="font-bold ml-1" href="/">Create an account</Link>
-              </Box>
+      <Box className="w-full h-full flex flex-col items-center justify-center">
+        <Box className="w-full px-16 flex flex-col box-border">
+          <Box>
+            <h3>Sign In</h3>
+            <Box className="mt-4 text-xs">
+              <span>New user?</span>
+              <Link className="font-bold ml-1" href="/">Create an account</Link>
             </Box>
-
-            <TextField value={username.value} label="Username" className="mt-5" onChange={event => username.value = event.target.value} />
-            <TextField value={password.value} label="Password" type="password" className="mt-5" onChange={event => password.value = event.target.value} />
-
-            <Box className="text-right text-xs mt-5">
-              <Link href="/">Forget password?</Link>
-            </Box>
-
-            <LoadingButton loading={loading} variant="contained" size="large" className="mt-5" onClick={handleSignIn}>Sign In</LoadingButton>
           </Box>
+
+          <TextField value={username.value} label="Username" className="mt-5" onChange={event => username.value = event.target.value} />
+          <TextField value={password.value} label="Password" type="password" className="mt-5" onChange={event => password.value = event.target.value} />
+
+          <Box className="text-right text-xs mt-5">
+            <Link href="/">Forget password?</Link>
+          </Box>
+
+          <LoadingButton loading={loading} variant="contained" size="large" className="mt-5" onClick={handleSignIn}>Sign In</LoadingButton>
         </Box>
       </Box>
     </UserLayout>
