bb39a682628f263dd96dcc9f02bbc4e905acf481
zekun.jin <zekun.jin@connext.com.cn>
2022-11-03 14:54:42 +0800

    feat: add router guard

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,24 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { utilsModule } from './common/modules/utils/utils.module'
+import { cookie } from './common/providers/cookie.provider'
+import { CookieKey } from './common/enums/cookie-key.enum'
 
 export const WHITELIST = ['/user/sign-in']
 
 export function middleware(request: NextRequest) {
+  const { server: { get: getCookie } } = utilsModule.useExport(cookie)
+  const token = getCookie(request.headers.get('cookie') || '', CookieKey.ACCESS_TOKEN)
+
+  if (WHITELIST.includes(request.nextUrl.pathname))
+    return NextResponse.next()
+
+  if (token)
+    return NextResponse.next()
+
   return NextResponse.redirect(new URL('/user/sign-in', request.url))
 }
 
 export const config = {
-  matcher: '/'
+  matcher: '/((?!api|_next/static|favicon.ico).*)'
 }
